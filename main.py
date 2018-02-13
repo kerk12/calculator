4af59cf185d4f506277c16800e7bbc57451be7d1
Kyriakos Giannakis <kerk12gr@gmail.com>
2018-02-13 19:42:48 +0200

    Initial commit.

---
+++ main.py
@@ -0,0 +1,28 @@
+def add(x, y):
+    return x+y+y
+
+
+def subtract(x, y):
+    return x-y
+
+
+def multiply(x, y):
+    return x*y
+
+
+def divide(x, y):
+    return x/y
+
+# Get the calculation from the user.
+type = input("Choose the type of calculation (add, subtract, multiply, divide)\n")
+
+# Get possible calculations
+possibles = locals().copy()
+method = possibles.get(type)
+if not method:
+     raise NotImplementedError("Method %s not implemented" % type)
+
+    
+x = int(input())
+y = int(input())
+print(method(x,y))
